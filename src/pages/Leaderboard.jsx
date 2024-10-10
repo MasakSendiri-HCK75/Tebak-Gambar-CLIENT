4e6d13c1cbc44be0b4671a1da833ac0764c7ffcc
kiolaksono <kiolaksono@gmail.com>
2024-10-10 12:41:25 +0700

    Setting Leaderboard page

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -16,9 +16,34 @@ export default function Leaderboard() {
 
   return (
     <div>
-        <h1>Leader Board</h1>
+        
         {leader?.length === 0 ? (
-            <h1>Loading...</h1>
+            <div
+            className="w-screen h-screen bg-cover bg-no-repeat bg-center flex justify-center items-start"
+            style={{
+              backgroundImage:
+                "url('https://comicvine.gamespot.com/a/uploads/original/11124/111241908/7724665-1eedc391-c316-4930-8aca-5abe21de148a.gif')",
+              paddingTop: "50px",
+            }}
+          >
+            <div className="w-3/4 bg-white bg-opacity-80 border-black border-2 rounded-lg shadow-lg p-6">
+              <table className="table w-full">
+                <thead>
+                  <tr className="bg-gray-800 text-white">
+                    <th className="px-4 py-2"></th>
+                    <th className="px-4 py-2">Name</th>
+                    <th className="px-4 py-2">Score</th>
+                    <th className="px-4 py-2">Time</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Loading</td>
+                    </tr>
+                </tbody>
+          </table>
+        </div>
+      </div>
         ) : (
     <>
       <Exit />
@@ -51,7 +76,7 @@ export default function Leaderboard() {
                   </th>
                   <td className="px-4 py-4 flex items-center border-b border-black">
                     <img
-                      src="https://example.com/user-avatar.jpg"
+                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5zv605IDYRA7f3I0IMFUy4BKP3ETSMleuJA&s"
                       alt="User Avatar"
                       className="w-8 h-8 rounded-full mr-2"
                     />
