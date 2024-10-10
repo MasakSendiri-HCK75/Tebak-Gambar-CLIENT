457e9a213c7c015f06b34ff7af035c0eddb2aef7
“Fahmi-ssy” <fahmihariadi896@gmail.com>
2024-10-10 10:44:07 +0700

    Design button exit and background

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -48,7 +48,7 @@ export default function HomePage() {
     <>
     <Exit/>
     
-      <div className="w-screen h-screen bg-[url('./assets/1.jpg')] bg-cover bg-no-repeat bg-center flex flex-col justify-center items-center ">
+      <div className="w-screen h-screen bg-[url('https://jan-schlosser.de/wp-content/uploads/z_Pixel-Art-Star-Wars-Animation.gif')] bg-cover bg-no-repeat bg-center flex flex-col justify-center items-center ">
         <div className="flex flex-row justify-center items-center space-x-10 mt-40">
           <Link to='/room'><button className="btn btn-lg w-80 h-40 bg-[url('/assets/multiplayer.png')] bg-cover bg-center text-white font-extrabold text-5xl rounded-lg shadow-lg transition-transform transform hover:scale-110 hover:shadow-xl duration-300 ease-in-out flex items-center justify-center">
             Multiplayer
