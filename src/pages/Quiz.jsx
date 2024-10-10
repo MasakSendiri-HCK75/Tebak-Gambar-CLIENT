1d54feed73f48619fbf141ad807423b61ae69c8f
“Fahmi-ssy” <fahmihariadi896@gmail.com>
2024-10-10 10:27:54 +0700

    Fixing quiz feature

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -105,7 +105,7 @@ export default function Quiz() {
     setTimeout(() => {
       setCurrentQuestion(currentQuestion + 1);
       setTimeLeft(10);
-    }, 2000); 
+    }, 1000); 
   };
 
   return (
